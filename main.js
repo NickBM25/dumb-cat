753bda9297d4e68cb3957398a6aeb86c7d232cbc
NickBM25 <120268916+NickBM25@users.noreply.github.com>
2023-05-07 00:34:19 -0300

    files
    added files, poggg

---
+++ main.js
@@ -0,0 +1,8 @@
+function verificarInput() {
+    var input = document.getElementById("input").value;
+    if (input.toLowerCase() === "oi") {
+        alert("vc cumprimentou o gatinho burro!!! ele está feliz :)");
+    } else {
+        alert("eu sou burro, vc precisa escrever perfeitamente");
+    }
+}
